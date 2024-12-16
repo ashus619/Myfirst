589ef861f9ad63dae39e1beb754e7cfd411082bd
ashus619 <aashutoshgaikwad89@gmail.com>
2024-12-16 19:13:11 +0530

    my_first.py

--- my_first.py
+++ my_first.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1g0J74dJoBW6acldEzX3jpS-2TVdsoaRg
 """
 
-!pip install streamlit
-
 import streamlit as st
 import numpy as np
 
